030da81f153cfdb5dafefb590328c5ea92861988
qiuchengw <qiuchengw@163.com>
2019-08-08 23:28:55 +0800

    tab 切换响应

--- src/app/components/tab-panel/tab-panel.component.ts
+++ src/app/components/tab-panel/tab-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NzTabChangeEvent } from 'ng-zorro-antd';
 
 
 interface WebTab{
@@ -16,6 +17,14 @@ export class TabPanelComponent implements OnInit {
   private _index = 0;
   private _id = 1;
   private _tabs: WebTab[] = [];
+
+  get currentWeb(){
+    if (this._tabs.length == 0 || this._index < 0 
+      || this._index >= this._tabs.length){
+      return null; 
+    }
+    return this.web(this._tabs[this._index].id);
+  }
 
   closeTab(id: number): void {
     for (let i = 0; i < this._tabs.length; ++i) {
@@ -41,9 +50,17 @@ export class TabPanelComponent implements OnInit {
     this._index = this._tabs.length - 1;
   }
 
+  web(id: number): any{
+    return document.querySelector(`#web-${id}`);
+  }
+
   navTo(url: string) {
     if (this._tabs.length === 0 || this._index < 0 || this._index > this._tabs.length) {
       return;
     }
+  }
+
+  onTabChanged(event: NzTabChangeEvent){
+    console.log("--> the current web:", this.currentWeb);
   }
 }
