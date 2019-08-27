3c1c4ab066c92225d94a75a171ab1a51433c3ff5
qiuchengw <qiuchengw@163.com>
2019-08-27 22:44:43 +0800

    + webpage 事件处理

--- main.ts
+++ main.ts
@@ -1,61 +1,84 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, screen, ipcMain } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
-let win, serve;
-const args = process.argv.slice(1);
-serve = args.some(val => val === '--serve');
+// 窗口管理器
+class WinManager {
+  private _topWin: BrowserWindow[] = [];
 
-function createWindow() {
+  constructor(private _serve: boolean){
 
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+  }
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-      webviewTag: true,
-    },
-  });
-
-  if (serve) {
-    require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`)
+  createWindow(): BrowserWindow {
+    const electronScreen = screen;
+    const size = electronScreen.getPrimaryDisplay().workAreaSize;
+  
+    // Create the browser window.
+    let win = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width: size.width,
+      height: size.height,
+      webPreferences: {
+        nodeIntegration: true,
+        webviewTag: true,
+      },
     });
-    win.loadURL('http://localhost:4200');
-  } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    this._topWin.push(win);
+  
+    if (this._serve) {
+      require('electron-reload')(__dirname, {
+        electron: require(`${__dirname}/node_modules/electron`)
+      });
+      win.loadURL('http://localhost:4200');
+    } else {
+      win.loadURL(url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+      }));
+    }
+  
+    if (this._serve) {
+      win.webContents.openDevTools();
+    }
+  
+    let that = this;
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      // Dereference the window object, usually you would store window
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      this.destroyWin(win);
+    });
+    return win;
   }
 
-  if (serve) {
-    win.webContents.openDevTools();
+  destroyWin(win: BrowserWindow){
+    const idx = this._topWin.indexOf(win);
+    if (idx >= 0){
+      this._topWin.splice(idx, 1);
+    }
   }
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-
+  hasWindow(){
+    return this._topWin.length > 0;
+  }
 }
 
-try {
+let _man: WinManager;
 
+try {
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+  app.on('ready', ()=>{
+    const args = process.argv.slice(1);
+    const serve = args.some(val => val === '--serve');
+    _man = new WinManager(serve);
+    _man.createWindow();
+  });
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
@@ -69,9 +92,17 @@ try {
   app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
+    if (!!_man && !_man.hasWindow()) {
+      _man.createWindow();
     }
+  });
+
+  ipcMain.on('new-window', (evt: Electron.IpcMainEvent, ...arg:any[])=>{
+    console.log("---> new-window evt, arg", evt, arg);
+    arg.forEach((t)=>{
+      console.log("---> new-window: naviage to url", t);
+      _man.createWindow();
+    })
   });
 
 } catch (e) {
--- src/app/shared/directives/webview/webview.directive.ts
+++ src/app/shared/directives/webview/webview.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef } from '@angular/core';
-import { WebviewTag } from 'electron';
+import { WebviewTag, shell, ipcRenderer } from 'electron';
 
 @Directive({
   selector: 'webview'
@@ -8,8 +8,39 @@ export class WebviewDirective {
 
   constructor(private _web: ElementRef<WebviewTag>) {
     console.log('---? theeee', _web);
-    this._web.nativeElement.addEventListener('load-commit', (t) => {
-      console.log('--->', t);
+    this.initEvents(this._web.nativeElement);
+  }
+
+  initEvents(web: WebviewTag) {
+    web.addEventListener('load-commit', (t) => {
+      console.log('--->load-commit', t);
+    });
+
+    web.addEventListener('did-finish-load', (t) => {
+      console.log('--->did-finish-load', t);
+    });
+
+    web.addEventListener('did-fail-load', (t) => {
+      console.log('--->did-fail-load', t);
+    });
+
+    web.addEventListener('new-window', /*async*/ (e) => {
+      console.log('--->new-window', e);
+      const protocol = require('url').parse(e.url).protocol;
+        if (protocol === 'http:' || protocol === 'https:') {
+          ipcRenderer.send('new-window', {
+            url: e.url,
+          });
+          // await shell.openExternal(e.url)
+        }
+    });
+
+    web.addEventListener('will-navigate', async (e) => {
+      console.log('--->will-navigate', e);
+    });
+
+    web.addEventListener('close', async (e) => {
+      console.log('--->close', e);
     });
   }
 
